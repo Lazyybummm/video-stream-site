c8fab35fc5a5b734465b6284c79b20d04fd2abb5
Yash choudhary <tanvichoudhary@tanvis-MacBook-Air.local>
2026-03-21 01:45:39 +0530

    made tweaks in the ui and endpoints

--- be-video/index.js
+++ be-video/index.js
@@ -1,41 +1,56 @@
+import "dotenv/config";
 import express, { json } from "express"
-import dotenv from "dotenv";
-dotenv.config();
+import bcrypt, { hash } from "bcrypt"
+import jwt from "jsonwebtoken"
+import pool from "./db.js";
 import axios from "axios"
+import https from "https"
 import cors from "cors"
 import getlanding from "./services/getLanding.js";
 import { Check } from "./cache/Redisops.js";
 import { Setex } from "./cache/Redisops.js";
+import redis from "./redisClient.js";
+
 const app=express()
 app.use(cors());
 app.use(express.json());
-let id_store=[{}]
+
+const tmdbAgent = new https.Agent({ keepAlive: true });
+
+let id_store=[]
 let landing_store=[];
+
+app.post("/signup",async (req,res)=>{
+    
+})
+
 app.get("/landing",async (req,res)=>{
     //using layered caching : in memory->redis server -> api call
+    const selectedMedia=req.query.selectedMedia
     const currentTime=Date.now()
-    const cached=await Check('landing')
-    if(landing_store[0] && landing_store[0].time-currentTime<900000){
-        return res.json({
-            trending:landing_store[0].trending,
-            popular:landing_store[0].popular,
-            top_rated:landing_store[0].top_rated
-        })
-    }
+    // const cached=await Check('landing')
+    // if(landing_store[0] && landing_store[0].time-currentTime<900000){
+    //     return res.json({
+    //         trending:landing_store[0].trending,
+    //         popular:landing_store[0].popular,
+    //         top_rated:landing_store[0].top_rated
+    //     })
+    // }
 
-    if(cached!=null){
-        landing_store[0]={time:currentTime,...cached}
-        return res.send(cached)
-    }
-
-    const data=await getlanding()
-    landing_store[0]={time:currentTime,...data}
-    await Setex('landing',3600,data)
+    // if(cached!=null){
+    //     landing_store[0]={time:currentTime,...cached}
+    //     return res.send(cached)
+    // }
+    console.log("fetching from api")
+    const data=await getlanding(tmdbAgent)
+    // landing_store[0]={time:currentTime,...data}
+    // await Setex('landing',3600,data)
     res.send(data);
 })
 
 app.get("/search",async (req,res)=>{
     let search_query=req.query.search_query//check the search query , it should be present and a string , convert it into lowercase 
+    const selectedMedia=req.query.selectedMedia//make sure the frontend selects the same (either movie or tv)
     search_query=search_query.toLowerCase();
     const filter=id_store.filter((m)=>{return m.search_query==search_query})
     if(filter.length!=0){
@@ -44,13 +59,14 @@ app.get("/search",async (req,res)=>{
     }
     //if not in filter then we'll resort to redis storage 
     else{
-        console.log("inside else")
     const response = await axios.get(
-        `https://api.themoviedb.org/3/search/movie?query=${search_query}&include_adult=false&language=en-US&page=1`,
+        `https://api.themoviedb.org/3/search/${selectedMedia}?query=${search_query}&include_adult=false&language=en-US&page=1`,
         {
+          httpsAgent: tmdbAgent,
+          timeout: 10000,
           headers: {
             Authorization: `Bearer ${process.env.TOKEN}`,
-          },
+          }, 
         }
       );
         //id_store.push({search_query:search_query,id:response.data.results[0].id})//lowercase one should be pushed to optimize cache hits 
@@ -59,15 +75,76 @@ app.get("/search",async (req,res)=>{
 })
 
 
+app.get("/details",async(req,res)=>{
+    const movie_id=req.query.id
+    const selectedMedia=req.query.selectedMedia
+    const response = await axios.get(
+        `https://api.themoviedb.org/3/${selectedMedia}/${movie_id}`,
+        {
+          httpsAgent: tmdbAgent,
+          headers: {
+            Authorization: `Bearer ${process.env.TOKEN}`,
+          }, 
+        }
+      );
+    //   res.json({
+    //     episode_count:response.data.number_of_episodes,
+    //     season_count:response.data.number_of_seasons
+    //   })
+    
+    res.send(response.data);
+})
+
+
 
 app.get("/recc",async (req,res)=>{
     const id =req.query.id
-    const response=await axios.get(`https://api.themoviedb.org/3/movie/${id}/recommendations`,{
+    let url
+    const selectedMedia=req.query.selectedMedia
+    selectedMedia=="movie"?url=`https://api.themoviedb.org/3/movie/${id}/recommendations`:url=`https://api.themoviedb.org/3/tv/${id}/recommendations`
+    const response=await axios.get(url,{
+        httpsAgent: tmdbAgent,
         headers:{
             Authorization: `Bearer ${process.env.TOKEN}`,
         }
     })
     res.send(response.data.results)
 })
+
+app.get("/watchlist",async (req,res)=>{
+    //extract an id , fetch all the movies user has watched from the watchlist table , send their metadata s required to the user 
+    const id=req.params.id//id is hardcoded for now 
+    const response=await pool.query("SELECT * FROM watchlist WHERE user_id = 1 ORDER BY added_at DESC;")
+    console.log(response);
+    res.send(response.rows)//gonna return an array , will loop it up at the frontend 
+
+})
+
+app.post("/watchlist",async (req,res)=>{
+    console.log("watchlist is hit")
+    const metadata=req.body.metadata
+    console.log("metadata:",metadata[0])
+    const query = `
+  INSERT INTO watchlist (user_id, movie_id, title, media_type, poster_path, release_date, metadata)
+  VALUES ($1, $2, $3, $4, $5, $6, $7)
+`;
+    const values = [
+    1, 
+    metadata[0].id, 
+    metadata[0].title || metadata[0].name, 
+    metadata[0].media_type || (metadata[0].first_air_date ? 'tv' : 'movie'), 
+    metadata[0].poster_path, 
+    metadata[0].release_date || metadata[0].first_air_date,
+    JSON.stringify(metadata[0]) // This saves the whole object into the JSONB column
+    ];
+    const response=await pool.query(query,values);
+   if(response.rowCount==1){
+    return res.send("movie added to watchlist")
+   }
+   else{
+    return res.send("some error occured try again later")
+   }
+})
+
 
 app.listen(3000);
--- be-video/services/getLanding.js
+++ be-video/services/getLanding.js
@@ -1,12 +1,32 @@
+import axios from "axios";
 
-import axios  from "axios";
+async function getlanding(agent) { // Receive the agent here
+    const config = {
+        httpsAgent: agent, // Use the persistent agent
+        timeout: 10000,
+        headers: { Authorization: `Bearer ${process.env.TOKEN}` }
+    };
 
-async function getlanding(){
-    const [trendingRes, topRatedRes, popularRes] = await Promise.all([
-        axios.get("https://api.themoviedb.org/3/trending/movie/day",{headers:{Authorization: `Bearer ${process.env.TOKEN}`} }),
-        axios.get("https://api.themoviedb.org/3/movie/top_rated", {headers:{Authorization: `Bearer ${process.env.TOKEN}`} }),
-        axios.get("https://api.themoviedb.org/3/movie/popular", {headers:{Authorization: `Bearer ${process.env.TOKEN}`} }),
-      ]);
-      return [trendingRes.data.results,topRatedRes.data.results,popularRes.data.results];
+    const [
+        trendingtvRes, topRatedtvRes, populartvRes,
+        trendingmovieRes, topRatedmovieRes, popularmovieRes
+    ] = await Promise.all([
+        axios.get(`https://api.themoviedb.org/3/trending/tv/day`, config),
+        axios.get(`https://api.themoviedb.org/3/tv/top_rated`, config),
+        axios.get(`https://api.themoviedb.org/3/tv/popular`, config),
+        axios.get(`https://api.themoviedb.org/3/trending/movie/day`, config),
+        axios.get(`https://api.themoviedb.org/3/movie/top_rated`, config),
+        axios.get(`https://api.themoviedb.org/3/movie/popular`, config),
+    ]);
+
+    return [
+        trendingtvRes.data.results, 
+        topRatedtvRes.data.results, 
+        populartvRes.data.results,
+        trendingmovieRes.data.results, 
+        topRatedmovieRes.data.results, 
+        popularmovieRes.data.results
+    ];
 }
+
 export default getlanding;
